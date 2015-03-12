4fe31be63796be5ce133f15a28c4081476508a1f
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-03-12 04:01:08 +0100

    Put file uploader inside a panel

--- src/client/js/components/FileUploader.jsx
+++ src/client/js/components/FileUploader.jsx
@@ -1,23 +1,28 @@
 const React = require('react');
 const Input = require('react-bootstrap/lib/Input');
+const Panel = require('react-bootstrap/lib/Panel');
 
 let FileUploader = React.createClass({
-  getDefaultProps: function() {
+  getDefaultProps: function () {
     return {
       label: 'Upload File'
     };
   },
   render() {
     return (
-      <form
-        className = "form-horizontal file-uploader"
-        id        = "uploadForm"
-        encType   = "multipart/form-data"
-        action    = "/upload"
-        method    = "post" >
-        <Input type="file" name="fileUpload" label="{this.props.label}" labelClassName="col-xs-2" wrapperClassName="col-xs-10"/>
-        <Input type="submit" defaultValue="Upload" name="submit" bsStyle="primary" labelClassName="col-xs-2" wrapperClassName="col-xs-10"/>
-      </form>
+      <div>
+        <Panel header={this.props.label}>
+          <form
+            className = "file-uploader"
+            id        = "uploadForm"
+            encType   = "multipart/form-data"
+            action    = "/upload"
+            method    = "post" >
+            <Input type="file" name="fileUpload" wrapperClassName="col-xs-6"/>
+            <Input type="submit" defaultValue="Upload" name="submit" bsStyle="primary" wrapperClassName="col-xs-6"/>
+          </form>
+        </Panel>
+      </div>
     );
   }
 });
